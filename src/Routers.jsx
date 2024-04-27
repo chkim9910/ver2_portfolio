9fd3fd0d0c75ae0aa52e8667679c067b15203f22
chohyang kim <chkim9910@naver.com>
2024-04-27 22:26:59 +0900

    feat: 404.html, script

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -1,5 +1,5 @@
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./views/home/Home";
 import Layout from "./components/layout/Layout";
 import X2y2 from "./views/x2y2/X2y2";
@@ -9,7 +9,7 @@ import Kawa from "./views/kawa/Kawa";
 
 const Routers = () => {
   return (
-    <Router>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
@@ -19,7 +19,7 @@ const Routers = () => {
           <Route path="/동물자유연대" element={<Kawa />} />
         </Route>
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 
