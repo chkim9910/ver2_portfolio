a2241e3d05e86f942c8e068a5890d118d360f3f6
chohyang kim <chkim9910@naver.com>
2024-04-28 04:19:01 +0900

    chore

--- src/Routers.jsx
+++ src/Routers.jsx
@@ -8,7 +8,7 @@ import Kawa from "./views/kawa/Kawa";
 
 const Routers = () => {
   return (
-    <BrowserRouter basename="/ver2_portfolio">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
